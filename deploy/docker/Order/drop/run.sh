5bf913eea8719c30f534667dab372c6e145586c0
jmaus67 <javierms5@hotmail.com>
2020-11-13 17:15:43 +0100

    TravisCI ahora crea imágenes con los binarios construidos y las sube a Docker Hub

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#no lanzar el .jar hasta que no esté accesible el servicio de MongoDB
+#el host 'mongo' tendrá una IP otorgada al lanzar la pila de #servicios,
+#y le damos el apodo 'mongo', utilizado aquí y en el #archivo application.properties
 
 while ! curl http://mongo:27017/
 do
@@ -7,4 +10,4 @@ do
 done
 echo "$(date) - connected successfully"
 
-java -jar ordering-*.jar
+java -jar usr/local/app/ordering-service-0.1.0.jar -spring.config.location=application.properties
